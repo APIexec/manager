5869c3da7fa969093a801b522d2eb4d30d10aa41
tom <tom@sapletta.com>
2018-05-09 00:24:21 +0200

    Installer, update, init

--- up.bat
+++ up.bat
@@ -1,15 +1,10 @@
 ::cd devops
-@ECHO OFF &SETLOCAL
-
-setlocal enableextensions enabledelayedexpansion
+::setlocal enableextensions enabledelayedexpansion
 set devops_path=%cd%\devops
 ::set firstLine=1
 for /f "delims=" %%i in (%HOMEPATH%\path.txt) do (
     ::if !firstLine!==1 set devops_path=%%i
     ::set firstLine=0
-    devops_path=%%i
+    set devops_path=%%i
 )
-::endlocal
-
-more %HOMEPATH%\path.txt
-more %devops_path%\doc\logo\soft.txt
+cd %devops_path%
